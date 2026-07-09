6e5cd0c6de43c5eef28250649221cb1199fb50cf
Josef Bauer <josef.bauer.1st@gmail.com>
2026-07-09 14:18:49 +0200

    Add PostgreSQL integration test setup and JetBrains datasource configuration

--- backend/src/test/kotlin/shop/voenix/testing/PostgresIntegrationTest.kt
+++ backend/src/test/kotlin/shop/voenix/testing/PostgresIntegrationTest.kt
@@ -1,19 +1,19 @@
 package shop.voenix.testing
 
-import shop.voenix.db.DatabaseSettings
 import io.ktor.server.config.MapApplicationConfig
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import org.testcontainers.postgresql.PostgreSQLContainer
 import org.testcontainers.utility.DockerImageName
+import shop.voenix.db.DatabaseSettings
 
 @Testcontainers(disabledWithoutDocker = true)
 abstract class PostgresIntegrationTest {
     protected fun postgresSettings(poolName: String): DatabaseSettings =
         DatabaseSettings(
             jdbcUrl = postgres.getJdbcUrl(),
-            username = postgres.getUsername(),
-            password = postgres.getPassword(),
+            username = postgres.username,
+            password = postgres.password,
             maximumPoolSize = 2,
             poolName = poolName,
         )
@@ -21,8 +21,8 @@ abstract class PostgresIntegrationTest {
     protected fun postgresConfig(): MapApplicationConfig {
         val config = MapApplicationConfig()
         config.put("database.jdbcUrl", postgres.getJdbcUrl())
-        config.put("database.username", postgres.getUsername())
-        config.put("database.password", postgres.getPassword())
+        config.put("database.username", postgres.username)
+        config.put("database.password", postgres.password)
         config.put("database.maximumPoolSize", "2")
         return config
     }
